d8b0ea712161b33cfb6b4e1020050ae4294f3aa4
sonubai <109574776+sonubai@users.noreply.github.com>
2022-07-25 17:01:59 +0530

    Update Progect1.java

--- src/main/java/Newpackage/Progect1.java
+++ src/main/java/Newpackage/Progect1.java
@@ -8,8 +8,7 @@ public class Progect1 {
 		System.out.println("priya");
 		System.out.println("priya");
 		System.out.println("amar");
-		System.out.println("poorna");
-		System.out.println("priya");
+		
 		
 		
 		
